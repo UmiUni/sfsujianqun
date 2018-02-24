b4475df9c504f98a52c10a074d89143d2b32d519
Chaoran Wang <superchaoran@gmail.com>
2018-02-24 06:48:58 +0000

    adding new group

--- tiantian.py
+++ tiantian.py
@@ -36,7 +36,7 @@ def sendGroupInviteMsg(msg,CurUserName):
   #print x
   if(len(x) >0):
     y= int(x[0])
-    if(y>=0 and y<=22):
+    if(y>=0 and y<=23):
       #print settings.chatGroups[y]
       if msg['FromUserName'] not in settings.admins:
         if(preventAbuseTalking(CurUserName)):
@@ -45,6 +45,9 @@ def sendGroupInviteMsg(msg,CurUserName):
       sleep(0.5)
     elif(y==99):
       advertiseQR(CurUserName)
+    elif(y==100):
+      pullMembersMore(msg, settings.chatGroups[23], CurUserName)
+      sleep(0.5)
   itchat.send_msg(settings.vT, CurUserName)
   sleep(0.5)
   msgText = msg['Text']
